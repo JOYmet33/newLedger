68c81b1369d9afbe3898a73fb3e86a7d4ba7a855
joymet33 <joymet33@gmail.com>
2024-05-30 15:51:20 +0900

    feat: (error) MonthNavigation.jsx styled-componenets

--- 2해설/src/Home.jsx
+++ 2해설/src/Home.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import MonthNavigation from "./components/MonthNavigation";
 
 const Container = styled.main`
   max-width: 800px;
@@ -9,7 +10,7 @@ const Container = styled.main`
   margin: 0 auto;
 `;
 
-const Section = styled.section`
+export const Section = styled.section`
   background-color: #ffffff;
   border-radius: 16px;
   padding: 20px;
@@ -18,8 +19,7 @@ const Section = styled.section`
 const Home = () => {
   return (
     <Container>
-      <h1>여기가 진짜 HOME</h1>
-      <Section>지출을 입력하는 섹션</Section>
+      <MonthNavigation />
       <Section>캘린더 역할을 하는 섹션</Section>
       <Section>지출을 리스팅하는 섹션</Section>
     </Container>
